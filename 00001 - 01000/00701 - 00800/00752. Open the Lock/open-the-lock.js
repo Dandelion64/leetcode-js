680e83150b75d6798f5fa99725751c18db7afff6
dandelion64 <coolpillow0308@gmail.com>
2023-01-31 00:09:25 +0800

    2023/01/30 nighttime

--- 00001 - 01000/00701 - 00800/00752. Open the Lock/open-the-lock.js
+++ 00001 - 01000/00701 - 00800/00752. Open the Lock/open-the-lock.js
@@ -68,19 +68,97 @@ const openLock = (deadends, target) => {
 
                 if (!visited.has(up)) {
                     queue.push(up);
-                    visited.add(up)
+                    visited.add(up);
                 }
 
                 const down = wheelDown(cur, j);
 
                 if (!visited.has(down)) {
                     queue.push(down);
-                    visited.add(down)
+                    visited.add(down);
                 }
             }
         }
 
         ++step;
+    }
+
+    return -1;
+};
+
+/* =========================================================
+=       Solution 2. bidirectional BFS: O(n)                =
+========================================================= */
+
+/**
+ * @param {string} string
+ * @param {number} index
+ * @return {string}
+ */
+const wheelUp = (string, index) => {
+    const chars = string.split('');
+
+    chars[index] = ++chars[index] % 10;
+
+    return chars.join('');
+}
+
+/**
+ * @param {string} string
+ * @param {number} index
+ * @return {string}
+ */
+const wheelDown = (string, index) => {
+    const chars = string.split('');
+
+    chars[index] = (--chars[index] + 10) % 10;
+
+    return chars.join('');
+}
+
+/**
+ * @param {string[]} deadends
+ * @param {string} target
+ * @return {number}
+ */
+const openLock = (deadends, target) => {
+    const deadendsSet = new Set(deadends);
+    let queueOne = new Set(['0000']);
+    let queueTwo = new Set([target]);
+    const visited = new Set();
+    let step = 0;
+
+    while (queueOne.size && queueTwo.size) {
+        let temp = new Set();
+
+        for (let cur of queueOne) {
+            if (deadendsSet.has(cur)) {
+                continue;
+            }
+
+            if (queueTwo.has(cur)) {
+                return step;
+            }
+
+            visited.add(cur);
+
+            for (let j = 0; j < 4; ++j) {
+                const up = wheelUp(cur, j);
+
+                if (!visited.has(up)) {
+                    temp.add(up);
+                }
+
+                const down = wheelDown(cur, j);
+
+                if (!visited.has(down)) {
+                    temp.add(down);
+                }
+            }
+        }
+
+        ++step;
+        [queueOne, queueTwo] = [queueTwo, temp];
     }
 
     return -1;
