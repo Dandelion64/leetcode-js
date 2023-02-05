d7ec127d80f140af7b2d9f4f1a4797402165ae6b
dandelion64 <coolpillow0308@gmail.com>
2023-02-05 22:16:13 +0800

    2023/02/05 nighttime

--- 00001 - 01000/00501 - 00600/00509. Fibonacci Number/fibonacci-number.js
+++ 00001 - 01000/00501 - 00600/00509. Fibonacci Number/fibonacci-number.js
@@ -31,7 +31,32 @@ const fib = (n) => {
 };
 
 /* =========================================================
-=       Solution 2. bottom-top dp iteration: O(n)          =
+=       Solution 2.v1. bottom-top dp iteration: O(n)       =
+========================================================= */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const fib = (n) => {
+    if (n === 0 || n === 1) {
+        return n;
+    }
+
+    const dp = Array(n);
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n - 1];
+}
+
+/* =========================================================
+=       Solution 2.v2. bottom-top dp iteration: O(n)       =
 ========================================================= */
 
 /**
