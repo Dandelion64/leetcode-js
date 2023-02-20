6e8ce5756ac0499d29b4c678c93bce078b1aa4eb
dandelion64 <coolpillow0308@gmail.com>
2023-02-20 09:41:59 +0800

    Add some comments.

--- 00001 - 01000/00701 - 00800/00797. All Paths From Source to Target/all-paths-from-source-to-target.js
+++ 00001 - 01000/00701 - 00800/00797. All Paths From Source to Target/all-paths-from-source-to-target.js
@@ -2,40 +2,7 @@
 =       Solution 1. backtracking: O(n * 2 ^ n)             =
 ========================================================= */
 
-/**
- * @param {number[][]} graph
- * @return {number[][]}
- */
-const allPathsSourceTarget = (graph) => {
-	const result = [];
-	const path = [0];
-
-	/**
-	 * @param {number[]} path
-	 */
-	const dfs = (path) => {
-		const cur = path.at(-1); // array.prototype.at()
-
-		if (cur === graph.length - 1) {
-			result.push([...path]);
-			return;
-		}
-
-		for (const v of graph[cur]) {
-			path.push(v);
-			dfs(path);
-			path.pop();
-		}
-	};
-
-	dfs(path);
-
-	return result;
-};
-
-/* =========================================================
-=       Solution 2. backtracking: O(n * 2 ^ n)             =
-========================================================= */
+// fast
 
 /**
  * @param {number[][]} graph
@@ -65,6 +32,41 @@ const allPathsSourceTarget = (graph) => {
 	};
 
 	traverse(graph, 0, path);
+
+	return result;
+};
+
+/* =========================================================
+=       Solution 2. backtracking: O(n * 2 ^ n)             =
+========================================================= */
+
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+const allPathsSourceTarget = (graph) => {
+	const result = [];
+	const path = [0];
+
+	/**
+	 * @param {number[]} path
+	 */
+	const dfs = (path) => {
+		const cur = path.at(-1); // array.prototype.at()
+
+		if (cur === graph.length - 1) {
+			result.push([...path]);
+			return;
+		}
+
+		for (const v of graph[cur]) {
+			path.push(v);
+			dfs(path);
+			path.pop();
+		}
+	};
+
+	dfs(path);
 
 	return result;
 };
