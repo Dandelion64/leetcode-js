1c42c7c0491ff68599b7d16e2611cb001c6db237
dandelion64 <coolpillow0308@gmail.com>
2023-03-07 23:26:25 +0800

    update

--- 00001 - 01000/00201 - 00300/00238. Product of Array Except Self/product-of-array-except-self.js
+++ 00001 - 01000/00201 - 00300/00238. Product of Array Except Self/product-of-array-except-self.js
@@ -1,5 +1,5 @@
 /* =========================================================
-=       Solution 1. prefix sum: O(n)                       =
+=       Solution 1. O(n)                                   =
 ========================================================= */
 
 /**
@@ -18,6 +18,33 @@ const productExceptSelf = (nums) => {
 	for (let i = nums.length - 1; i >= 0; --i) {
 		result[i] = right * result[i];
 		right *= nums[i];
+	}
+
+	return result;
+};
+
+/* =========================================================
+=       Solution 2. prefix sum: O(n)                       =
+========================================================= */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = (nums) => {
+	const result = Array(nums.length).fill(1);
+
+	for (let i = 1; i < nums.length; ++i) {
+		result[i] = result[i - 1] * nums[i - 1];
+	}
+
+	let carry = 1;
+
+	nums.push(1);
+
+	for (let i = result.length - 1; i >= 0; --i) {
+		carry *= nums[i + 1];
+		result[i] *= carry;
 	}
 
 	return result;
