d3de5869117ba4a1819bd40fad35652b8da9a46d
dandelion64 <coolpillow0308@gmail.com>
2023-03-08 09:54:35 +0800

    2023/03/08 daytime

--- 00001 - 01000/00801 - 00900/00875. Koko Eating Bananas/koko-eating-bananas.js
+++ 00001 - 01000/00801 - 00900/00875. Koko Eating Bananas/koko-eating-bananas.js
@@ -1,5 +1,5 @@
 /* =========================================================
-=       Solution 1. binary search [,]: O(n log m)          =
+=       Solution 1.v1. binary search [,]: O(n log m)       =
 ========================================================= */
 
 /**
@@ -30,6 +30,38 @@ const minEatingSpeed = (piles, h) => {
 		mid = (lo + hi) >>> 1;
 
 		if (couldEatThemAll(piles, h, mid)) {
+			hi = mid;
+		} else {
+			lo = mid + 1;
+		}
+	}
+
+	return lo;
+};
+
+/* =========================================================
+=       Solution 1.v2. binary search [,]: O(n log m)       =
+========================================================= */
+
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+const minEatingSpeed = (piles, h) => {
+	let lo = 1, hi = Math.max(...piles), mid;
+
+	while (lo < hi) {
+		mid = (lo + hi) >>> 1;
+
+        let time = 0;
+
+        for (let i = 0; i < piles.length; ++i) {
+            time += Math.ceil(piles[i] / mid);
+            if (time > h) break;
+        }
+
+		if (time <= h) {
 			hi = mid;
 		} else {
 			lo = mid + 1;
